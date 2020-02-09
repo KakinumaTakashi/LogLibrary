57d4c21bd98822d156efd822adefb28e59e8d67d
Takashi Kakinuma <kakinuma.takashi@homes.ecweb.jp>
2020-02-09 23:50:39 +0900

    タイムスタンプの出力を実装

--- LogLibrary/LogLibrary.cpp
+++ LogLibrary/LogLibrary.cpp
@@ -1,56 +1,84 @@
-// MathLibrary.cpp : Defines the exported functions for the DLL.
-#include "pch.h" // use stdafx.h in Visual Studio 2017 and earlier
-#include <utility>
-#include <limits.h>
+// プリコンパイル済みヘッダー ファイル
+#include "pch.h"
+
+#include <fstream>
 #include "LogLibrary.h"
 
-// DLL internal state variables:
-static unsigned long long previous_;  // Previous value, if any
-static unsigned long long current_;   // Current sequence value
-static unsigned index_;               // Current seq. position
 
-// Initialize a Fibonacci relation sequence
-// such that F(0) = a, F(1) = b.
-// This function must be called before any other function.
-void fibonacci_init(
-    const unsigned long long a,
-    const unsigned long long b)
+#ifdef _DEBUG
+#define DEBUG_LOG(fmt, ...) {char b[1024];sprintf_s(b, 1024, fmt, __VA_ARGS__);strcat_s(b, 1024, "\n");OutputDebugString(b);}
+#else
+#define DEBUG_LOG(fmt, ...) {}
+#endif // DEBUG
+
+
+static std::string log_file_path;
+static std::ofstream log_output_stream;
+
+int log_open(std::string file_path)
 {
-    index_ = 0;
-    current_ = a;
-    previous_ = b; // see special case when initialized
+    // ログファイルのオープン
+    log_output_stream.open(file_path, std::ios::app);
+
+    if (!log_output_stream) {
+        // ファイルオープン失敗
+        return ERROR_OPEN_FAILED;
+    }
+    // ファイル名保存
+    log_file_path = file_path;
+
+    return ERROR_SUCCESS;
 }
 
-// Produce the next value in the sequence.
-// Returns true on success, false on overflow.
-bool fibonacci_next()
+void log_flush()
 {
-    // check to see if we'd overflow result or position
-    if ((ULLONG_MAX - previous_ < current_) ||
-        (UINT_MAX == index_))
-    {
-        return false;
+    if (!log_output_stream) return;
+    log_output_stream.flush();
+}
+
+void log_close(bool flush)
+{
+    if (!log_output_stream) return;
+
+    if (flush) {
+        log_flush();
     }
 
-    // Special case when index == 0, just return b value
-    if (index_ > 0)
-    {
-        // otherwise, calculate next sequence value
-        previous_ += current_;
-    }
-    std::swap(current_, previous_);
-    ++index_;
-    return true;
+    log_output_stream.close();
 }
 
-// Get the current value in the sequence.
-unsigned long long fibonacci_current()
+std::string get_current_time_string()
 {
-    return current_;
+    SYSTEMTIME system_time;
+    GetLocalTime(&system_time);
+
+    char buffer[24];
+    sprintf_s(buffer, 24, "%04d/%02d/%02d %02d:%02d:%02d.%03d",
+        system_time.wYear, system_time.wMonth, system_time.wDay,
+        system_time.wHour, system_time.wMinute, system_time.wSecond, system_time.wMilliseconds);
+
+    return std::string(buffer);
 }
 
-// Get the current index position in the sequence.
-unsigned fibonacci_index()
+void log_write(std::string format, ...)
 {
-    return index_;
+    va_list args;
+    va_start(args, format);
+
+    char arg_format_buffer[4096];
+    vsnprintf(arg_format_buffer, 4096, format.c_str(), args);
+
+    char write_format_buffer[4096];
+    snprintf(write_format_buffer, 4096, "%s\n", get_current_time_string().c_str());
+    DEBUG_LOG("len=%d", strlen(write_format_buffer));
+
+    log_output_stream.write(write_format_buffer, strlen(write_format_buffer));
+
+    va_end(args);
+}
+
+
+std::string get_current_thread_id_string()
+{
+    return "";
 }
--- LogLibraryTP/LogLibraryTP.cpp
+++ LogLibraryTP/LogLibraryTP.cpp
@@ -5,15 +5,7 @@
 
 int main()
 {
-    // Initialize a Fibonacci relation sequence.
-    fibonacci_init(1, 1);
-    // Write out the sequence values until overflow.
-    do {
-        std::cout << fibonacci_index() << ": "
-            << fibonacci_current() << std::endl;
-    } while (fibonacci_next());
-    // Report count of values written before overflow.
-    std::cout << fibonacci_index() + 1 <<
-        " Fibonacci sequence values fit in an " <<
-        "unsigned 64-bit integer." << std::endl;
+	log_open("C:\\Work\\test.log");
+	log_write("%s", "テスト");
+	log_close(true);
 }
